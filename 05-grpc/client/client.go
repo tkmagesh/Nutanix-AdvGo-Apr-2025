9c9313a0b879d4409ba174e8b595d89cf6471007
Magesh Kuppan <tkmagesh77@gmail.com>
2025-04-22 13:59:07 +0530

    server streaming

--- 05-grpc/client/client.go
+++ 05-grpc/client/client.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"fmt"
 	"grpc-app/proto"
+	"io"
 	"log"
+	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -18,13 +20,46 @@ func main() {
 	}
 	serviceClient := proto.NewAppServiceClient(clientConn)
 	ctx := context.Background()
+	// doRequestResponse(ctx, serviceClient)
+	doServerStreaming(ctx, serviceClient)
+
+}
+
+func doRequestResponse(ctx context.Context, serviceClient proto.AppServiceClient) {
+	timeoutCtx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+
 	req := &proto.AddRequest{
 		X: 100,
 		Y: 200,
 	}
-	res, err := serviceClient.Add(ctx, req)
+	res, err := serviceClient.Add(timeoutCtx, req)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	fmt.Println(res.GetResult())
+}
+
+func doServerStreaming(ctx context.Context, serviceClient proto.AppServiceClient) {
+	req := &proto.PrimeRequest{
+		Start: 2,
+		End:   100,
+	}
+	clientStream, err := serviceClient.GeneratePrimes(ctx, req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for {
+		res, err := clientStream.Recv()
+		if err == io.EOF {
+			fmt.Println("All the generated prime numbers have been received")
+			break
+		}
+		if err == nil {
+			fmt.Printf("Prime No : %d\n", res.GetPrimeNo())
+			continue
+		}
+		log.Fatalln(err)
+		break
+	}
 }
--- 05-grpc/server/server.go
+++ 05-grpc/server/server.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"grpc-app/proto"
 	"log"
 	"net"
+	"time"
 
 	"google.golang.org/grpc"
 )
@@ -16,10 +18,41 @@ type AppServiceImpl struct {
 func (asi *AppServiceImpl) Add(ctx context.Context, req *proto.AddRequest) (*proto.AddResponse, error) {
 	x := req.GetX()
 	y := req.GetY()
+	fmt.Printf("Processing x = %d, y = %d\n", x, y)
+	time.Sleep(5 * time.Second)
 	result := x + y
+	fmt.Printf("Sending result = %d\n", result)
 	return &proto.AddResponse{
 		Result: result,
 	}, nil
+}
+
+func (asi *AppServiceImpl) GeneratePrimes(req *proto.PrimeRequest, serverStream proto.AppService_GeneratePrimesServer) error {
+	start := req.GetStart()
+	end := req.GetEnd()
+	fmt.Printf("Generating Prime number from %d to %d\n", start, end)
+	for no := start; no <= end; no++ {
+		if isPrime(no) {
+			res := &proto.PrimeResponse{
+				PrimeNo: no,
+			}
+			fmt.Printf("Sending Prime No : %d\n", no)
+			if err := serverStream.Send(res); err != nil {
+				log.Fatalln(err)
+			}
+			time.Sleep(300 * time.Millisecond)
+		}
+	}
+	return nil
+}
+
+func isPrime(no int64) bool {
+	for i := int64(2); i <= (no / 2); i++ {
+		if no%i == 0 {
+			return false
+		}
+	}
+	return true
 }
 
 func main() {
